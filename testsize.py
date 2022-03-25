4fb04703fba148ac1c4e7596c3a1a3960e38432f
Core i99 <thehackguy@icloud.com>
2022-03-25 17:00:56 +0100

    Delete testsize.py

--- testsize.py
+++
@@ -1,8 +0,0 @@
-import os
-# Proxmox ISO folder: /var/lib/vz/template/iso
-
-SizeBytes = os.path.getsize('C:/Users/Stijn Rombouts/BigSurRec.img') 
-
-SizeKiloBytes = str(SizeBytes/1024).strip(".0") #remove .0 from calculation
-
-print('Size of file is', SizeKiloBytes, 'bytes')
